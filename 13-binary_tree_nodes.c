94ca343947ec6b93332debc3bbc943bd03613fe9
Yohannes251 <yohannes.z251@gmail.com>
2021-12-01 19:10:21 +0000

    Updated conditions

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,15 +10,11 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t left_nodes, right_nodes;
 
-	if (!tree)
+	if (!tree || (!tree->left && !tree->right))
 		return (0);
 
-	if (tree->left && tree->right)
-		return (2);
-	else
-		return (1);
 	left_nodes = binary_tree_nodes(tree->left);
 	right_nodes = binary_tree_nodes(tree->right);
 
-	return (left_nodes + right_nodes);
+	return (left_nodes + right_nodes + 1);
 }
