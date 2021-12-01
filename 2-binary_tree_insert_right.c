2fdf942e30fc57705b3e2fb219222b32fe529f04
Yohannes251 <yohannes.z251@gmail.com>
2021-12-01 09:33:49 +0000

    Changed condition position

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,9 +9,14 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = binary_tree_node(parent, value);
+	if (parent == NULL)
+		return (NULL);
 
-	if ((new_node == NULL) || (parent == NULL))
+	binary_tree_t *new_node;
+
+	new_node = binary_tree_node(parent, value);
+
+	if (new_node == NULL)
 		return (NULL);
 
 	if (parent->right != NULL)
