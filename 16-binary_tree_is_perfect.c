7bc2c99d8e5ab091506a3c9a7dc8ee4043ae799a
Yohannes251 <yohannes.z251@gmail.com>
2021-12-02 22:58:15 +0000

    Added a fullness check

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -23,6 +23,27 @@ size_t binary_tree_height(const binary_tree_t *tree)
 }
 
 /**
+ * binary_tree_is_full - Checks if a binary tree is full
+ * @tree: pointer to the root node of the tree
+ * Return: 1 if it is full or 0 otherwise
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+
+	left = binary_tree_is_full(tree->left);
+	right = binary_tree_is_full(tree->right);
+
+	return (MIN(left, right));
+}
+
+/**
  * binary_tree_is_perfect - Checks if a binary tree is perfect
  * @tree: pointer to the root node of the tree
  * Return: 1 if it is perfect or 0 otherwise
@@ -44,5 +65,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 
-	return (left == right ? 1 : 0);
+	if (left == right)
+		if (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right))
+			return (1);
+	return (0);
 }
