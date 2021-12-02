e328ddd06298f264eac1aef86f80d584dc329b00
Yohannes251 <yohannes.z251@gmail.com>
2021-12-02 22:20:58 +0000

    Added conditions for edge cases

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -35,6 +35,12 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
+	if (!tree->left && !tree->right)
+		return (1);
+
+	if (!tree->left || !tree->right)
+		return (0);
+
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 
